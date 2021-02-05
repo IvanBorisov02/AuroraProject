55085fd88b9108995df297b368d6f74be1522771
Ivan Borisov <i.borisov12@outlook.com>
2021-02-05 17:35:00 +0200

    Add files via upload

---
+++ Web/AS.Web/Areas/Identity/Pages/Account/Logout.cshtml.cs
@@ -0,0 +1,28 @@
+﻿using System.Threading.Tasks;
+using AS.Data.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace AS.Web.Areas.Identity.Pages.Account
+{
+    [AllowAnonymous]
+    public class LogoutModel : PageModel
+    {
+        private readonly SignInManager<ASUser> _signInManager;
+
+        public LogoutModel(SignInManager<ASUser> signInManager)
+        {
+            _signInManager = signInManager;
+        }
+
+        public async Task<IActionResult> OnGet()
+        {
+            await _signInManager.SignOutAsync();
+
+            return Redirect("/");
+        }
+
+    }
+}
